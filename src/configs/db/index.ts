c5d2cf9fabeb95f5a30ae2ee06e0a778a76bd7a0
toandq <toandauquoc.vccorp.vn>
2023-02-11 08:45:05 +0700

    ...

--- src/configs/db/index.ts
+++ src/configs/db/index.ts
@@ -8,11 +8,13 @@ async function connect() {
         //     useCreateIndex: true,
         //     useFindAndModify: false,
         // })
-         await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@leaning-app.zvik9qk.mongodb.net/learning_app?useNewUrlParser=true&useUnifiedTopology=true&useCreateIndex=true&useFindAndModify=false`);
+        await mongoose.connect(
+            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@leaning-app.zvik9qk.mongodb.net/learning_app?useNewUrlParser=true&useUnifiedTopology=true&useCreateIndex=true&useFindAndModify=false`
+        )
         console.log('Connect db successfully!!!')
     } catch (error) {
         console.log('Connect db failure!!!')
     }
 }
-
+/* ... */
 export default { connect }
